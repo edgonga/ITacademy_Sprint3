b130ea5d4380994675903da13cf300e77e8812bb
Joaquim <jsabria2@xtec.cat>
2023-04-25 13:05:27 +0200

    ts initials

---
+++ 3.1_Patterns/app/app.ts
@@ -0,0 +1,27 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const inbox: string = path.join(__dirname, './inbox')
+const outbox: string = path.join(__dirname, './outbox')
+
+function reverseText(text: string): string {
+    return text.split('').reverse().join('');
+  }
+
+async function processFiles(): Promise<void> {
+  try {
+
+    const files: string[] = await fs.readdir(inbox);
+    for (const file of files) {
+      const data: string = await fs.readFile(path.join(inbox, file), 'utf8');
+      await fs.writeFile(path.join(outbox, file), reverseText(data));
+      console.log(`${file} was successfully saved in the outbox!`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+processFiles();
+
+

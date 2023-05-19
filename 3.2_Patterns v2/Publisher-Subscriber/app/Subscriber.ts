c878b7aa75e1740bb7b6afc93da9a621c94b766f
Edu Gonzalez Garcia <johndoe@example.com>
2023-05-19 19:39:08 +0200

    test pub-subs v1

---
+++ 3.2_Patterns v2/Publisher-Subscriber/app/Subscriber.ts
@@ -0,0 +1,40 @@
+import * as amqp from 'amqplib'
+
+export class Subscriber {
+
+    private connection: amqp.Connection
+    private channel: amqp.Channel
+    private queueName: string
+    private teacherName: string
+
+    constructor(queueName: string, teacherName: string) {
+        this.queueName = queueName
+        this.teacherName = teacherName
+    }
+
+    public async connectToMoodle(): Promise<void> {
+        this.connection = await amqp.connect('amqp://localhost')
+        this.channel = await this.connection.createChannel()
+        await this.channel.assertQueue(this.queueName, { durable: false })
+    }
+
+    public async reviewingNews(): Promise<void> {
+        console.log("Checking if there is new homeworks to correct...")
+
+        this.channel.consume(this.queueName, (message) => {
+            if(message !== null) {
+                console.log(`New task received: ${message.content.toString()}`)
+                this.channel.ack(message)
+                
+            }
+        })
+    }
+
+    public async disconnect(): Promise<void> {
+        await Promise.all([
+            this.channel.close(),
+            this.connection.close()
+        ])
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ITacademy_Sprint3
 Barcelona Activa 3rd sprint of the bootcamp BA Node.js
+
+Take note that the "Publisher-Subscriber" project needs RabbitMQ to be executed
+That's why I prepare this project to be launch with Docker without downloading and installing RabbitMQ
+To run the container:
+
+```js
+docker run -d --name mi-contenedor-rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq_publisher-subscriber
+```
+
+For more details, the image container base is built in nodejs and rabbitMQ is build above it and check the Dockerfile for the init commands
